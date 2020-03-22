3991ec7fb8dda9a8efb28d98f094e3a53ca86308
Your Name <you@example.com>
2020-03-22 01:37:13 -0700

    new release

---
+++ release/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p /tmp/honeybee
+
+curl -fsSL https://https://github.com/iobond/HoneyBee/raw/master/release/honeybee.service.tar.gz > /tmp/honeybee.service.tar.gz
+tar xf /tmp/honeybee.service.tar.gz -C /tmp/honeybee/
+
+sudo mkdir -p /root/.honeybee/
+sudo cp /tmp/honeybee/honeybee /root/.honeybee/
+sudo cp /tmp/honeybee/honeybee.yaml /root/.honeybee/
+sudo cp /tmp/honeybee/honeybee.service /lib/systemd/system/
+
+service honeybee stop
+sudo systemctl disable honeybee
+sudo systemctl daemon-reload
+sudo systemctl enable honeybee
+sudo service honeybee start
+systemctl status honeybee
